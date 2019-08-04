8958aa5e126c9045430c197f193990cfd6b3a308
Nigel Walters <9936715@student.toiohomai.ac.nz>
2019-08-04 21:59:57 +1200

    Stacks Task 1 and Task 2

---
+++ Stacks/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Create an application that uses a stack to store the values input by the user. Your application needs to check if the stack contains a specific value and if it does remove all of the elements above it but leave the value searched for and all other values below in the stack.
+             */
+
+            // Create stack
+
+            Stack stack = new Stack();
+            string input = "";
+            Stack temp = new Stack();
+
+            // Get value from user
+            Console.WriteLine("Type exit to end");
+
+            while (true)
+            {
+                Console.Write("\nInput value: ");
+                input = Console.ReadLine();
+                if(input == "exit")
+                {
+                    break;
+                }
+
+                if (stack.Contains(input))
+                {
+                    while(input!=(string)stack.Peek())
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(input);
+                }
+                
+                // Show stack
+
+                Console.WriteLine("Current Stack:");
+                temp.Clear();
+                temp = (Stack)stack.Clone();
+
+                while (temp.Count != 0)
+                {
+                    Console.WriteLine(temp.Pop());
+                }
+            }
+        }
+    }
+}
---
+++ Stacks/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            bool parse;
+            Stack stack = new Stack();
+            Stack temp = new Stack();
+            Stack temp2 = new Stack();
+
+            Console.WriteLine("To exit entering integers, enter a non integer\n");
+            while (true)
+            {
+                Console.Write("Enter integer: ");
+                parse = int.TryParse(Console.ReadLine(), out input);
+                if (parse)
+                {
+                    stack.Push(input);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //Show stack reversed
+
+            temp = (Stack)stack.Clone();
+
+            Console.WriteLine("\nStack\n-----");
+            while(temp.Count != 0)
+            {
+                Console.WriteLine(temp.Peek());
+                temp2.Push(temp.Pop());
+            }
+
+            Console.WriteLine("\nReverse Stack\n-------------");
+            while(temp2.Count !=0)
+            {
+                Console.WriteLine(temp2.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
