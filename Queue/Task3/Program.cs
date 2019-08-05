ab4da2d54a518bb0e0e0dab0b24abdba2f3a5dd2
Nigel Walters <9936715@student.toiohomai.ac.nz>
2019-08-05 16:37:56 +1200

    Queues Completed

---
+++ Queue/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            Queue sorted = new Queue();
+
+            q.Enqueue("Nigel");
+            q.Enqueue("Makuini");
+            q.Enqueue("Jerome");
+            q.Enqueue("Makaio");
+            q.Enqueue("Zane");
+
+            string[] names = new string[q.Count];
+
+            q.CopyTo(names, 0);
+
+            Array.Sort(names);
+
+            foreach(string s in names)
+            {
+                sorted.Enqueue(s);
+            }
+
+            Console.WriteLine("Sorted");
+            while (sorted.Count != 0)
+            {
+                Console.WriteLine(sorted.Dequeue());
+            }
+
+
+
+        }
+    }
+}
