2ab82e60e0f6811a57dfb003b1c793862b146b33
Nigel Walters <9936715@student.toiohomai.ac.nz>
2019-08-05 16:18:15 +1200

    Queues Task 1 and 2

---
+++ Queue/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue("Nigel");
+            q.Enqueue("Makuini");
+            q.Enqueue("Jerome");
+            q.Enqueue("Makaio");
+            q.Enqueue("Zane");
+
+            while (q.Count != 0)
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+        }
+    }
+}
---
+++ Queue/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue("Nigel");
+            q.Enqueue("Makuini");
+            q.Enqueue("Jerome");
+            q.Enqueue("Makaio");
+            q.Enqueue("Zane");
+
+            // Check to see if name is in queue
+            Console.Write("Enter a name to search: ");
+            string input = Console.ReadLine();
+
+            if (q.Contains(input))
+            {
+                Console.WriteLine("Name is in the queue");
+            }
+            else
+            {
+                Console.WriteLine("Name is not found");
+            }
+        }
+    }
+}
